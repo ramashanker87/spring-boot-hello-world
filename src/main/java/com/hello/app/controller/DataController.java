d9d67e3eb7a7c0df1299daf99c2eebb52dc916dd
admin <rama@gail.com>
2025-03-03 23:14:23 -0800

    added new controller

--- src/main/java/com/hello/app/controller/DataController.java
+++ src/main/java/com/hello/app/controller/DataController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataController {
-
     @GetMapping("/data")
     public String dataApi(){
         return "Data Controler";
